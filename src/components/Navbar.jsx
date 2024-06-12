a57030ef447f7611ab000fa563f4293239efcc47
karann09 <karankumar011kp@gmail.com>
2024-06-12 12:46:01 +0530

    logo updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,12 @@
-import logo from "../assets/kevinRushLogo.png";
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 const Navbar = () => {
   return (
-    <nav className="mb-20 flex items-center justify-between py-6">
+    <nav className="mb-15 flex items-center justify-between py-6">
         <div className="flex flex-shrink-0 items-center">
-            <img className="mx-2 w-10" src={logo} alt="logo" />
+            <h3 className=" flex mx-2 w-10 font-bold text-2xl">Kz</h3>
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl">
             <FaLinkedin/>
